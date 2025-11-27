4a2e502ab54b9857c785e611d96e2d2f7bdc06fe
bloom-1 <saifeddine.bousaada28@gmail.com>
2025-11-27 17:42:47 +0100

    1 step of the project

---
+++ chapters/chapter_3.py
@@ -0,0 +1,3 @@
+"""Chapter 3 – Classes and discovering Hogwarts:
+• Learning basic spells: 1 defensive spell, 1 oXensive spell, 3 utility spells.
+• Mini-games related to Snape's classes (logic, memory, spell recognition, etc.)."""
