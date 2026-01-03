a64be33f8a4ab2be7badd2b3a57446dc80944559
bloom-1 <saifeddine.bousaada28@gmail.com>
2026-01-03 11:33:30 +0100

    1 step of the project

--- chapters/chapter_3.py
+++ chapters/chapter_3.py
@@ -1,7 +1,7 @@
+import random
 from utils.input_utils import load_file, ask_text
-from random import randint
 from universe.house import update_house_points, display_winning_house
-from universe.character import display_character
+from universe.character import display_character, add_item
 
 def learn_spells(character, file_path="data/spells.json"):
     print("You begin your magic lessons at Hogwarts...")
@@ -11,41 +11,54 @@ def learn_spells(character, file_path="data/spells.json"):
     defensive = []
     offensive = []
 
-    for sp in spells:
+    i = 0
+    while i < len(spells):
+        sp = spells[i]
         if sp["type"] == "Utility":
             utility.append(sp)
         elif sp["type"] == "Defensive":
             defensive.append(sp)
         elif sp["type"] == "Offensive":
             offensive.append(sp)
+        i += 1
 
     learned = []
 
-    i = randint(0, len(defensive) - 1)
-    learned.append(defensive[i])
 
-    i = randint(0, len(offensive) - 1)
-    learned.append(offensive[i])
+    learned.append(defensive[randint(0, len(defensive) - 1)])
+    learned.append(offensive[randint(0, len(offensive) - 1)])
 
     picked = 0
     while picked < 3:
-        i = randint(0, len(utility) - 1)
-        if utility[i] not in learned:
-            learned.append(utility[i])
+        sp = utility[randint(0, len(utility) - 1)]
+        if sp not in learned:
+            learned.append(sp)
             picked += 1
 
-    if "Spells" not in character:
-        character["Spells"] = []
 
-    for sp in learned:
-        character["Spells"].append(sp["name"])
-        print("You have just learned:", sp["name"], "(", sp["type"], ")")
+    i = 0
+    while i < len(learned):
+        sp = learned[i]
+        add_item(character, "Spells", sp["name"])
+        print("You have just learned:", sp["name"], "(" + sp["type"] + ")")
         input("Press Enter to continue...")
+        i += 1
+
+    i = 0
+    while i < len(learned):
+        sp = learned[i]
+        add_item(character, "Spells", sp["name"])
+        print("You have just learned:", sp["name"], "(" + sp["type"] + ")")
+        input("Press Enter to continue...")
+        i += 1
 
     print("You have completed your basic spell training at Hogwarts!")
     print("Here are the spells you now master:")
-    for name in character["Spells"]:
-        print("-", name)
+    i = 0
+    while i < len(character["Spells"]):
+        print("-", character["Spells"][i])
+        i += 1
+    input()
 
 
 def magic_quiz(character, file_path="data/magic_quiz.json"):
@@ -59,8 +72,8 @@ def magic_quiz(character, file_path="data/magic_quiz.json"):
     correct = 0
 
     count = 0
-    while count < 4:
-        i = randint(0, len(quiz) - 1)
+    while len(asked) < 4:
+        i = random.randint(0, len(quiz) - 1)
         if i in asked:
             continue
         asked.append(i)
@@ -77,7 +90,6 @@ def magic_quiz(character, file_path="data/magic_quiz.json"):
             print("Wrong answer. The correct answer was:", a)
 
         input()
-        count += 1
 
     points = correct * 25
     print("Quiz finished! You earned", points, "points.")
